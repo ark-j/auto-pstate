837c3acb5e95627aabdf1c4e3e09e4b8ba4d5f25
jayesh <jayesh6297@gmail.com>
2023-11-14 16:36:32 +0530

    added dbus based event processing rather than polling sysfs

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+go mod download
 go build -o auto-pstate -ldflags="-s -w" main.go
 sudo cp auto-pstate /usr/bin/ -v
 sudo cp auto-pstate.service /etc/systemd/system/ -v
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
 	"runtime"
 	"strings"
-	"time"
+
+	"github.com/godbus/dbus/v5"
 )
 
 const (
@@ -15,25 +17,13 @@ const (
 	scalingDriverPath = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_driver"
 	governerPath      = "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor"
 	eppPath           = "/sys/devices/system/cpu/cpu%d/cpufreq/energy_performance_preference"
-	batPath           = "/sys/class/power_supply/AC/online"
+	upowerPath        = "/org/freedesktop/UPower/devices/line_power_AC"
 )
 
 func main() {
 	IsRoot()
 	IsPState()
-	// sets the state variable to opposite of charging for first run
-	state := false
-	if !Charging() {
-		state = true
-	}
-	// after first run whenever charhing changes so does our epp-state
-	for true {
-		if state != Charging() {
-			SetState()
-			state = Charging()
-		}
-		time.Sleep(5 * time.Second)
-	}
+	SetState()
 }
 
 // checks if script is run by root or not
@@ -47,33 +37,24 @@ func IsRoot() {
 func IsPState() {
 	b, err := os.ReadFile(scalingDriverPath)
 	if err != nil {
-		log.Fatal("[WARNING] file does not exists for scaling driver", err)
+		slog.Error("file does not exists for scaling driver", err)
 	}
 	if strings.TrimSpace(string(b)) != "amd-pstate-epp" {
-		log.Fatal("[WARNING] system is not running amd-pstate-epp")
+		slog.Error("system is not running amd-pstate-epp")
 	}
 }
 
-// check if system on charging or on battery
-func Charging() bool {
-	b, err := os.ReadFile(batPath)
-	if err != nil {
-		log.Fatal("[WARNING] file not found for AC")
-	}
-	return strings.TrimSpace(string(b)) == "1"
-}
-
-// set the pwoersave governer if not already set
+// set the powersave governer if not already set
 func SetGoverner() {
 	b, err := os.ReadFile(fmt.Sprintf(governerPath, 0))
 	if err != nil {
-		log.Println("[WARNING] governer file does not exists")
+		slog.Warn("governer file does not exists")
 	}
 	if string(b) != "powersave" {
 		for i := 0; i < runtime.NumCPU(); i++ {
 			if err := os.WriteFile(fmt.Sprintf(governerPath, i),
 				[]byte("powersave"), os.ModePerm); err != nil {
-				log.Printf("[ERROR] while setting powersave governer to cpu core %d err -> %v\n", i, err)
+				slog.Error(fmt.Sprintf("while setting powersave governer to cpu core %d err -> %v\n", i, err))
 				continue
 			}
 		}
@@ -91,13 +72,39 @@ func SetEPP(val string) {
 	}
 }
 
-// set the proper state based on charging
+// listens for he dbus event and sets the performance governer
 func SetState() {
-	if Charging() {
-		SetEPP(eppStateAC)
-		fmt.Println("[INFO] epp state set to balance_performance")
-	} else {
-		SetEPP(eppStateBat)
-		fmt.Println("[INFO] epp state set to power")
+	// after first run whenever charhing changes so does our epp-state
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		slog.Error("unable to connect to system bus", err)
+		return
+	}
+	defer conn.Close()
+
+	signal := make(chan *dbus.Signal, 10)
+	conn.Signal(signal)
+
+	if err = conn.AddMatchSignal(
+		dbus.WithMatchObjectPath(dbus.ObjectPath(upowerPath)),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	for msg := range signal {
+		if msg.Name == "org.freedesktop.DBus.Properties.PropertiesChanged" {
+			m, ok := msg.Body[1].(map[string]dbus.Variant)
+			if ok {
+				switch m["Online"].Value().(bool) {
+				case true:
+					SetEPP(eppStateAC)
+					fmt.Println("[INFO] epp state set to balance_performance")
+				case false:
+					SetEPP(eppStateBat)
+					fmt.Println("[INFO] epp state set to power")
+				}
+			}
+
+		}
 	}
 }
