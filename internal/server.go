4d6e26b64d9211b3cda9dfdba44db829e7275aab
Jayesh Kale <jayesh6297@gmail.com>
2024-12-23 06:32:52 +0530

    server code boilerplate

--- internal/server.go
+++ internal/server.go
@@ -1,17 +1,54 @@
 package internal
 
-type Server struct{}
+import (
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+)
 
-func NewServer() *Server {
-	return &Server{}
+const SockPath = "/var/run/epp.sock"
+
+type Server struct {
+	mux     *http.ServeMux
+	srv     *http.Server
+	listner net.Listener
 }
 
-func (srv *Server) Start() {}
+func NewServer() *Server {
+	var err error
+	srv := &Server{}
+	srv.listner, err = net.Listen("unix", SockPath)
+	if err != nil {
+		slog.Error(fmt.Sprintf("unable to start unix socket listner: %v", err))
+		os.Exit(1)
+	}
+	srv.mux = http.NewServeMux()
+	srv.srv = &http.Server{
+		Handler: srv.mux,
+	}
+	return srv
+}
 
-func (srv *Server) Close() {}
+// Start starts the server recommended to launch it in goroutine
+func (srv *Server) Start() {
+	srv.routes()
+	if err := srv.srv.Serve(srv.listner); err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+}
 
-func (srv *Server) ManualHandler() {}
+func (srv *Server) Close() {
+	srv.srv.Close()
+	srv.listner.Close()
+}
 
-func (srv *Server) AutoHandler() {}
+func (srv *Server) routes() {}
 
-func (srv *Server) TimerHandler() {}
+func (srv *Server) ManualHandler(w http.ResponseWriter, r *http.Request) {}
+
+func (srv *Server) AutoHandler(w http.ResponseWriter, r *http.Request) {}
+
+func (srv *Server) TimerHandler(w http.ResponseWriter, r *http.Request) {}
